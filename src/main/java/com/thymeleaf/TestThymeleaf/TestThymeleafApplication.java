f877d266161b9322a4033054e9b2235cf25b1353
jplemonias <jean-paul.lemonias@le-campus-numerique.fr>
2022-09-20 10:13:24 +0200

    commit srping created

---
+++ src/main/java/com/thymeleaf/TestThymeleaf/TestThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.thymeleaf.TestThymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestThymeleafApplication.class, args);
+	}
+
+}
