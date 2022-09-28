0dd775fff93b9112baeb395c8f9902d0fde1c8d8
jplemonias <jean-paul.lemonias@le-campus-numerique.fr>
2022-09-28 16:34:23 +0200

    OK

--- src/main/java/com/thymeleaf/TestThymeleaf/TestThymeleafApplication.java
+++ src/main/java/com/thymeleaf/TestThymeleaf/TestThymeleafApplication.java
@@ -2,11 +2,15 @@ package com.thymeleaf.TestThymeleaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class TestThymeleafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestThymeleafApplication.class, args);
+		System.out.println("Thymeleaf loaded !");
 	}
 }
--- src/main/java/com/thymeleaf/TestThymeleaf/model/User.java
+++ src/main/java/com/thymeleaf/TestThymeleaf/model/User.java
@@ -1,6 +1,11 @@
 package com.thymeleaf.TestThymeleaf.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+  @Id
     private int id;
     private String name;
     private String championType;
